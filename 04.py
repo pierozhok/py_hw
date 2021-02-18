2620fd32b2ea15ef8d6cfd3dd778309986060fe2
pierozhok <73384862+pierozhok@users.noreply.github.com>
2021-02-18 18:11:26 +0300

    Create 04.py

---
+++ 04.py
@@ -0,0 +1,16 @@
+str = input('введите строку:')
+res = ''
+for s in str:
+    if s.isdigit():
+        res += s
+    else:
+        if res.endswith(' '):
+            pass
+        else:
+            res += ' '
+sum = 0
+lst = res.split(' ')
+for s in lst:
+    sum += int(s)
+print(sum)
+        

b501d1a7b2762da11d7467a3c779e90478b7451d
pierozhok <73384862+pierozhok@users.noreply.github.com>
2021-02-22 15:15:08 +0300

    Create 09.py

---
+++ 09.py
@@ -0,0 +1,4 @@
+problem 6 - print(sum([x for x in range(1,100)])^2 - sum([x^2 for x in range(1,100)]))
+problem 9 - 
+problem 40 - 
+problem 48 - print(str(sum([x*x for x in range(1,1001)]))[-10::])

18326e6b981b9a9dfa7aac79af91c98ba7987810
pierozhok <73384862+pierozhok@users.noreply.github.com>
2021-02-22 15:37:41 +0300

    Create 11.py

---
+++ 11.py
@@ -0,0 +1,4 @@
+alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def letters_range(a, b, c=1):
+    print(alph[alph.index(a):alph.index(b):c])
